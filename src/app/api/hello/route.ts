e19ba9ea8f01f88c9e7cf6f63562439fe45235d7
carrymycross7 <threet.ldustin@gmail.com>
2024-08-29 00:37:48 -0500

    fix vercel deploy error

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,12 +4,12 @@ type ResponseData = {
     message: string
 }
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseData>
-) {
-    res.status(200).json({ message: 'Hello from Next.js!' })
-}
+// export default function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<ResponseData>
+// ) {
+//     res.status(200).json({ message: 'Hello from Next.js!' })
+// }
 
 export async function GET() {
     return Response.json({ message: 'Hello from Next.js! I have a new message' })
