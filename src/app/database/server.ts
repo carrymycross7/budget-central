35e4e21e72d18c27deafa9fbd77a73555cfa1b1f
carrymycross7 <threet.ldustin@gmail.com>
2024-08-29 00:12:53 -0500

    12-13 create db connection, connection to postgres

--- src/app/database/server.ts
+++ src/app/database/server.ts
@@ -1,3 +1,12 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 
+const db = require('./config/dbConn.ts');
+
+const PORT = process.env.SERVER_PORT;
+
+const SEQUELIZE = db.connect();
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}.`)
+});
